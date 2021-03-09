3b8e559297cb37c9c0ed5207be251977ac51d849
fedcalderon <fedcalderon@gmail.com>
2021-03-09 17:29:00 -0500

    chapter 12 material, making ship move

---
+++ src/chapter12/starting_game_concepts/settings.py
@@ -0,0 +1,10 @@
+# This class contains the settings for the Alien Invasion Game
+
+class Settings:
+    def __init__(self):
+        # Screen Settings
+        self.screen_width = 600
+        self.screen_height = 400
+        self.bg_color = (230,230, 230)
+        # Ship Settings
+        self.ship_speed = 2.5
